6e1dcb67ea69df43938eb9ab6c6fbcda4f490818
trevor-mill <trevor.million@chicagobooth.edu>
2022-02-01 17:01:24 +0000

    workin

--- app/controllers/math_controller.rb
+++ app/controllers/math_controller.rb
@@ -5,5 +5,10 @@ class MathController < ApplicationController
     render({ :template => "math_calculations/addition_form.html.erb"})
   end
 
+  def wizard_add
+    
+
+    render({ :template => "math_calculations/addition_results.html.erb"})
+  end
 
 end
