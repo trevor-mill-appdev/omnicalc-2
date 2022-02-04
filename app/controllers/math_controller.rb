f2923dfd0616f5d63dd07160b45ce6cb1ed15ec2
trevor-mill <trevor.million@chicagobooth.edu>
2022-02-04 01:44:57 +0000

    add works

--- app/controllers/math_controller.rb
+++ app/controllers/math_controller.rb
@@ -6,9 +6,30 @@ class MathController < ApplicationController
   end
 
   def wizard_add
-    
+    @first_num = params[:first_num].to_f
+    @second_num = params[:second_num].to_f
+    @result = @first_num + @second_num
 
     render({ :template => "math_calculations/addition_results.html.erb"})
   end
 
+  def subtract_form
+
+    render({ :template => "math_calculations/subtract_form.html.erb"})
+  end
+
+  def wizard_subtract
+
+    render({ :template => "math_calculations/subtract_results.html.erb"})
+  end
+
+  def wizard_multiply
+
+    render({ :template => "math_calculations/multiply_results.html.erb"})
+  end
+
+  def wizard_divide
+
+    render({ :template => "math_calculations/divide_results.html.erb"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   get("/add", { :controller => "math", :action => "add_form"})
   get("/wizard_add", { :controller => "math", :action => "wizard_add"})
 
+  get("/wizard_subtract", { :controller => "math", :action => "wizard_subtract"})
+
+  get("/wizard_multiply", { :controller => "math", :action => "wizard_multiply"})
+
+  get("/wizard_divide", { :controller => "math", :action => "wizard_divide"})
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
