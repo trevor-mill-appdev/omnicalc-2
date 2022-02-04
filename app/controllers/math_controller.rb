286b53a5510c6d4085ecb2d7f549d019bdd5762d
trevor-mill <trevor.million@chicagobooth.edu>
2022-02-04 02:00:30 +0000

    multiply works

--- app/controllers/math_controller.rb
+++ app/controllers/math_controller.rb
@@ -26,7 +26,15 @@ class MathController < ApplicationController
     render({ :template => "math_calculations/subtract_results.html.erb"})
   end
 
+  def multiply_form
+
+    render({ :template => "math_calculations/multiply_form.html.erb"})
+  end
   def wizard_multiply
+
+    @first_num = params[:first_num].to_f
+    @second_num = params[:second_num].to_f
+    @result = @first_num * @second_num
 
     render({ :template => "math_calculations/multiply_results.html.erb"})
   end
